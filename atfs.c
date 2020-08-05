2001d4108c5b1cd42db927b18a2e5f3a53dffb91
Enze Li <lienze2010@126.com>
2020-08-05 12:00:52 +0800

    added atfs_mount function
    Signed-off-by: Enze Li <lienze2010@126.com>

--- atfs.c
+++ atfs.c
@@ -3,11 +3,36 @@
 #include <linux/fs.h>
 #include <linux/mount.h>
 
-static struct vfsmount *atfs_mount;
+static struct vfsmount *atfs_mnt;
+
+static int atfs_set_super(struct super_block *sb, void *data)
+{
+	sb->s_fs_info = data;
+	return set_anon_super(sb, data);
+}
+
+static struct dentry *atfs_mount(struct file_system_type *fs_type, int flags,
+		   const char *dev_name, void *data)
+{
+	struct super_block *sb = NULL;
+	struct inode *inode;
+	struct dentry *root;
+	printk(KERN_INFO "atfs_mount...");
+	sb = sget(fs_type, NULL, atfs_set_super, flags, NULL);
+	if (!sb) {
+		printk(KERN_INFO "atfs super_block is NULL");
+		return ERR_PTR(-EINVAL);
+	}
+	inode = new_inode(sb);
+	root = d_make_root(inode);		
+	sb->s_root = root;
+	return dget(sb->s_root);
+}
 
 static struct file_system_type atfs_fs_type = {
 	.name	= "atfs",
 	.owner	= THIS_MODULE,
+	.mount	= atfs_mount,
 };
 
 ssize_t atfs_file_read(struct file *filp, char __user *buf, 
@@ -29,13 +54,18 @@ int atfs_open_file(struct inode *inode, struct file *filp)
 
 struct dentry *atfs_create_file(const char* name)
 {
-	struct dentry *root_dentry = atfs_mount->mnt_sb->s_root;
-	struct inode *inode = new_inode(root_dentry->d_inode->i_sb);
+	struct dentry *root_dentry = NULL;
+	struct inode *inode = NULL;
+	BUG_ON(!atfs_mnt);
+	BUG_ON(!atfs_mnt->mnt_sb);
+	BUG_ON(!atfs_mnt->mnt_sb->s_root);
+	root_dentry = atfs_mnt->mnt_sb->s_root;
+	inode = new_inode(root_dentry->d_inode->i_sb);
 	if (root_dentry) {
 		d_instantiate(root_dentry, inode);
 		dget(root_dentry);
 	}
-	return NULL;
+	return ERR_PTR(-EINVAL);
 }
 
 struct dentry *atfs_create_dir(const char *name)
@@ -60,9 +90,11 @@ int atfs_register(void)
 		printk(KERN_ERR "register file system fail %d", ret);
 		return ret;
 	} else {
-		atfs_mount = kern_mount(&atfs_fs_type);
-		if (IS_ERR(atfs_mount)) {
-			printk(KERN_ERR "mount filesystem error %d", IS_ERR(atfs_mount));
+		printk(KERN_INFO "register file system succeed %d", ret);
+		atfs_mnt = kern_mount(&atfs_fs_type);
+		if (IS_ERR(atfs_mnt)) {
+			printk(KERN_ERR "mount filesystem error %ld", PTR_ERR(atfs_mnt));
+			return PTR_ERR(atfs_mnt);
 		}
 	}
 	atfs_create_dir("testDir");
